62907e27c11b8eb871f5eedb2e8850ddc9dd8b5c
Nabara99 <rtaufiq212@gmail.com>
2024-01-17 15:28:06 +0800

    orderby_dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -28,6 +28,9 @@ class DashboardController extends Controller
             ->join('programs', 'kegiatans.program_id', '=', 'programs.id')
             ->selectRaw('sum(pagu) as nilai, nama_sub, kode_sub, kode_kegiatan, kode_program')
             ->groupBy('nama_sub', 'kode_sub', 'kode_kegiatan', 'kode_program')
+            ->orderBy('programs.kode_program', 'asc')
+            ->orderBy('kegiatans.kode_kegiatan', 'asc')
+            ->orderBy('subs.kode_sub', 'asc')
             ->get();
 
         return view('pages.dashboard', compact('anggarans'));
